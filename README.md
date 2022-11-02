e69b42aca985aa541d3827a11e156bf6f1dce964
Ayush227 <76936278+Ayush227@users.noreply.github.com>
2022-11-02 17:59:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-vhff7b
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-vhff7b)
---
+++ src/calculator.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+  render() {
+    return 'GeeksforGeeks Calculator';
+  }
+}
+
+export default Calculator;
